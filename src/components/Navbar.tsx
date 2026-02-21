98677ee08b46f6823b88aa12340ae0cc2e8a18db
Omar <danieljomarvc@gmail.com>
2026-02-21 20:45:16 -0300

    set admin corregido

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ interface NavbarProps {
   role?: string | null;
 }
 
-const Navbar: React.FC<NavbarProps> = ({ role }) => {
+const Navbar: React.FC<NavbarProps> = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
